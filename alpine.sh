ab00d281814e124bf39b33114d40de7f495dc509
Cutsin <cutsin@gmail.com>
2018-09-28 01:21:36 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -11,14 +11,16 @@ sysctl -w net.ipv4.tcp_congestion_control=hybla
 sysctl -w net.ipv4.tcp_fastopen=3
 
 # Repositories
-echo "http://dl-3.alpinelinux.org/alpine/edge/community\nhttp://dl-3.alpinelinux.org/alpine/edge/main\nhttp://dl-3.alpinelinux.org/alpine/v3.7/community\nhttps://alpine-repo.sourceforge.io/packages" >> /etc/apk/repositories
+echo -e "http://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/v3.7/community\nhttps://alpine-repo.sourceforge.io/packages" >> /etc/apk/repositories
 wget -P /etc/apk/keys https://alpine-repo.sourceforge.io/DDoSolitary@gmail.com-00000000.rsa.pub
 
 # Firewall
-apk add iptalbes ca-certificates libressl
+apk add iptables ca-certificates libressl
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p udp --dport 443 -j ACCEPT
+/etc/init.d/iptables save
+service iptables start
 
 # bbr
 echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" > /etc/sysctl.conf
