875b28545332def6d35b7d967aaf46c855716917
Cutsin <cutsin@gmail.com>
2018-09-27 22:55:45 +0800

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -5,9 +5,7 @@ chmod +x bbr.sh
 # bbr
 echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" > /etc/sysctl.conf
 sysctl -p
-# ss
-wget https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo -P /etc/yum.repos.d/
-yum install shadowsocks-libev
+
 # vi /etc/shadowsocks-libev/config.json
 systemctl start shadowsocks-libev
 systemctl disable firewalld
@@ -15,3 +13,9 @@ systemctl disable firewalld
 # 
 echo 'fs.file-max = 6553560' > /etc/sysctl.conf
 echo '* soft nofile 65535\n* hard nofile 65535\n* soft nproc 65535\n* hard nproc 65535' > /etc/security/limits.conf
+
+# SS
+wget https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo -P /etc/yum.repos.d/
+wget https://copr.fedorainfracloud.org/coprs/outman/shadowsocks-libev/repo/epel-7/outman-shadowsocks-libev-epel-7.repo -P /etc/yum.repos.d/
+yum install -y epel-release
+yum install -y libsodium shadowsocks-libev simple-obfs
