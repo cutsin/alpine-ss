2c2e4af68ac02a838a92b7d4e7015dfaeade2466
Cutsin <cutsin@gmail.com>
2021-09-19 00:05:30 +0800

    Update trojan.onekey.sh

--- trojan.onekey.sh
+++ trojan.onekey.sh
@@ -53,7 +53,7 @@ cat>/srv/trojan.config.json<<EOF
   ],
   "log_level": 2,
   "ssl": {
-    "cert": "/srv/ssl/cert",
+    "cert": "/srv/ssl/fullchain",
     "key": "/srv/ssl/key",
     "key_password": "",
     "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
